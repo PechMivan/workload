c8473bba2c35f77d99f24730e1b8d7578b9619bd
PechMivan <pech.mivan@gmail.com>
2024-03-11 13:03:37 -0600

    added controller for workload

--- src/main/java/epam/microservice/workload/controllers/WorkloadController.java
+++ src/main/java/epam/microservice/workload/controllers/WorkloadController.java
@@ -1,6 +1,10 @@
 package epam.microservice.workload.controllers;
 
 import epam.microservice.workload.dto.ModifyWorkloadRequest;
+import epam.microservice.workload.entities.Workload;
+import epam.microservice.workload.mappers.WorkloadMapper;
+import epam.microservice.workload.services.WorkloadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/workloads")
+@RequiredArgsConstructor
 public class WorkloadController {
 
+    private final WorkloadMapper workloadMapper;
+    private final WorkloadService workloadService;
 
     @PostMapping
     public ResponseEntity<HttpStatus> modifyWorkload(ModifyWorkloadRequest request){
-
+        Workload workload = workloadMapper.modifyWorkloadRequestToWorkload(request);
+        if (request.actionType.equalsIgnoreCase("add")) workloadService.addWorkload(workload);
+        if (request.actionType.equalsIgnoreCase("delete")) workloadService.deleteWorkload(workload);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
