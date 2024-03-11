d2795378d41acace0a3ead7b2b0938818b38b5ae
PechMivan <pech.mivan@gmail.com>
2024-03-11 17:31:22 -0600

    added eureka client and discoverability

--- src/main/java/epam/microservice/workload/WorkloadApplication.java
+++ src/main/java/epam/microservice/workload/WorkloadApplication.java
@@ -2,8 +2,10 @@ package epam.microservice.workload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkloadApplication {
 
 	public static void main(String[] args) {
