67e61b63bd407e94453e5387e6f9def3fe6a6e61
PechMivan <pech.mivan@gmail.com>
2024-03-12 13:56:47 -0600

    added filter for transaction id header

--- src/main/java/epam/microservice/workload/controllers/WorkloadController.java
+++ src/main/java/epam/microservice/workload/controllers/WorkloadController.java
@@ -7,13 +7,11 @@ import epam.microservice.workload.mappers.WorkloadMapper;
 import epam.microservice.workload.services.WorkloadService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/workloads")
