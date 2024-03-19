55722d20b6dd0fd716352d985737b7401d10a4a2
PechMivan <pech.mivan@gmail.com>
2024-03-19 16:23:31 -0600

    added functionality to the receiver class to handle transactionID header and removed TransactionIdFilter and FilterConfig classes

--- src/main/java/epam/microservice/workload/receivers/WorkloadReceiverService.java
+++ src/main/java/epam/microservice/workload/receivers/WorkloadReceiverService.java
@@ -6,8 +6,14 @@ import epam.microservice.workload.mappers.WorkloadMapper;
 import epam.microservice.workload.services.WorkloadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -15,14 +21,29 @@ import org.springframework.stereotype.Component;
 public class WorkloadReceiverService {
 
     private static final String WORKLOAD_QUEUE = "workload.queue";
+    private static final String TRANSACTION_ID_HEADER = "Transaction-ID";
     private final WorkloadMapper workloadMapper;
     private final WorkloadService workloadService;
 
     @JmsListener(destination = WORKLOAD_QUEUE)
-    public void updateWorkload(ModifyWorkloadRequest request){
+    public void updateWorkload(@Payload ModifyWorkloadRequest request,
+                               @Header(name = TRANSACTION_ID_HEADER) String transactionId){
         log.info("received a message from {}", WORKLOAD_QUEUE);
+        extractTransactionIdHeader(transactionId);
+        log.info("Received a message from {} with {} action type",
+                WORKLOAD_QUEUE,
+                request.getActionType()
+        );
         Workload workload = workloadMapper.modifyWorkloadRequestToWorkload(request);
         if (request.getActionType().equalsIgnoreCase("add")) workloadService.addHours(workload);
         else if (request.getActionType().equalsIgnoreCase("delete")) workloadService.removeHours(workload);
+    }
+
+    private void extractTransactionIdHeader(String transactionId){
+        if (StringUtils.hasText(transactionId)) {
+            MDC.put(TRANSACTION_ID_HEADER, transactionId);
+        } else {
+            MDC.put(TRANSACTION_ID_HEADER, UUID.randomUUID().toString());
+        }
     }
 }
