10689b155bf9fa7a6fa370dd2412a28feee33200
PechMivan <pech.mivan@gmail.com>
2024-03-19 14:03:21 -0600

    added logic processing after receiving message

--- src/main/java/epam/microservice/workload/receivers/WorkloadReceiverService.java
+++ src/main/java/epam/microservice/workload/receivers/WorkloadReceiverService.java
@@ -1,6 +1,9 @@
 package epam.microservice.workload.receivers;
 
 import epam.microservice.workload.dto.ModifyWorkloadRequest;
+import epam.microservice.workload.entities.Workload;
+import epam.microservice.workload.mappers.WorkloadMapper;
+import epam.microservice.workload.services.WorkloadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
@@ -12,10 +15,14 @@ import org.springframework.stereotype.Component;
 public class WorkloadReceiverService {
 
     private static final String WORKLOAD_QUEUE = "workload.queue";
+    private final WorkloadMapper workloadMapper;
+    private final WorkloadService workloadService;
 
     @JmsListener(destination = WORKLOAD_QUEUE)
     public void updateWorkload(ModifyWorkloadRequest request){
         log.info("received a message from {}", WORKLOAD_QUEUE);
-        
+        Workload workload = workloadMapper.modifyWorkloadRequestToWorkload(request);
+        if (request.getActionType().equalsIgnoreCase("add")) workloadService.addHours(workload);
+        else if (request.getActionType().equalsIgnoreCase("delete")) workloadService.removeHours(workload);
     }
 }
